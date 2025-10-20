a0c16146cf27ef55dba3e3b8c22a6b14055c113f
Mahmoud Eldandrwi <mahmoudnoob597@gmail.com>
2025-10-20 23:05:02 +0300

    Implement mute button in PlayerGUI
    Added mute functionality to the PlayerGUI, including a mute button that toggles audio on and off.

--- PlayerGUI.cpp
+++ PlayerGUI.cpp
@@ -5,11 +5,15 @@ PlayerGUI::PlayerGUI()
     addAndMakeVisible(loadButton);
     addAndMakeVisible(restartButton);
     addAndMakeVisible(stopButton);
+    addAndMakeVisible(muteButton);
+
     addAndMakeVisible(volumeSlider);
 
     loadButton.addListener(this);
     restartButton.addListener(this);
     stopButton.addListener(this);
+    muteButton.addListener(this);
+
 
     volumeSlider.setRange(0.0, 1.0, 0.01);
     volumeSlider.setValue(0.5);
@@ -31,6 +35,7 @@ void PlayerGUI::resized()
     loadButton.setBounds(20, y, 100, 40);
     restartButton.setBounds(140, y, 80, 40);
     stopButton.setBounds(240, y, 80, 40);
+    muteButton.setBounds(40, 150, 80, 40);
 
     volumeSlider.setBounds(20, 100, getWidth() - 40, 30);
 }
@@ -45,12 +50,29 @@ void PlayerGUI::buttonClicked(juce::Button* button)
     else if (button == &restartButton)
     {
         if (audioPlayer)
+            audioPlayer->stop();
             audioPlayer->start();
     }
     else if (button == &stopButton)
     {
         if (audioPlayer)
             audioPlayer->stop();
+    }
+    else if (button == &muteButton)
+    {
+        if (audioPlayer) {
+			if (isMuted) {
+				volumeSlider.setValue(lastVolumeBeforeMute);
+				isMuted = false;
+                muteButton.setButtonText("Mute");
+            }
+			else {
+				lastVolumeBeforeMute = (float)volumeSlider.getValue();
+				volumeSlider.setValue(0.0);
+				isMuted = true;
+                muteButton.setButtonText("Unmute");
+            }
+        }
     }
 }
 
@@ -59,3 +81,4 @@ void PlayerGUI::sliderValueChanged(juce::Slider* slider)
     if (slider == &volumeSlider && audioPlayer)
         audioPlayer->setGain((float)slider->getValue());
 }
+
