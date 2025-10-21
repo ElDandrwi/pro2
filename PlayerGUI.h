387f583ef6bea44869588ef93792518bea3401f4
MoumenKhaled35 <momenkhaled66666@gmail.com>
2025-10-21 19:42:44 +0300

    Update PlayerGUI.h

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -5,7 +5,8 @@
 
 class PlayerGUI : public juce::Component,
     public juce::Button::Listener,
-    public juce::Slider::Listener
+    public juce::Slider::Listener,
+    public juce::Timer
 {
 public:
     PlayerGUI();
@@ -16,28 +17,29 @@ public:
 
     void setAudioPlayer(PlayerAudio* player) { audioPlayer = player; }
 
-   
     std::function<void()> onLoadFileRequest;
 
 private:
     juce::TextButton loadButton{ "Load Files" };
-	juce::TextButton muteButton{ "Mute" };
- juce::TextButton playButton{ "Play" };
- juce::TextButton pauseButton{ "Pause" };
- juce::TextButton gotoStartButton{ "|◄ Start" };
- juce::TextButton gotoEndButton{ "End ►|" };
+    juce::TextButton muteButton{ "Mute" };
+    juce::TextButton playButton{ "Play" };
+    juce::TextButton pauseButton{ "Pause" };
+    juce::TextButton gotoStartButton{ "Start" };
+    juce::TextButton gotoEndButton{ "End" };
+    juce::TextButton loopButton{ "Loop: Off" };
     juce::Slider volumeSlider;
+    juce::Slider positionSlider;
+    juce::Label timeLabel;
 
     PlayerAudio* audioPlayer = nullptr;
 
     float lastVolumeBeforeMute = 0.5f;
-
-	bool isMuted = false;
+    bool isMuted = false;
 
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
+    void timerCallback() override;
+    void updateTimeDisplay();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
 };
-
-
