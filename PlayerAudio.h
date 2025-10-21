149b258b53af0b77abdd1c90c2fc7bcb3b95bdeb
MoumenKhaled35 <momenkhaled66666@gmail.com>
2025-10-21 19:41:54 +0300

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -8,26 +8,32 @@ public:
     PlayerAudio();
     ~PlayerAudio() override;
 
-    // JUCE audio callbacks
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
-    // Basic controls
     void loadFile(const juce::File& file);
     void setGain(float gain);
- void play();
-	void pause();   
- void setPosition(double seconds);
- double getLengthInSeconds() const;
-	void goToStart();
-	void goToEnd();
+    void play();
+    void pause();
+    void setPosition(double seconds);
+    double getLengthInSeconds() const;
+    void goToStart();
+    void goToEnd();
+
+    void setLooping(bool shouldLoop);
+    bool isLooping() const { return isLoopEnabled; }
+
+    double getCurrentPosition() const;
+    double getPositionRatio() const;
 
 private:
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
 
+    bool isLoopEnabled = false;
+    double totalLengthInSeconds = 0.0;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
 };
-
