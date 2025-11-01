f8a1caf270b29c86f1b082032c2165ba21912acf
MoumenKhaled35 <momenkhaled66666@gmail.com>
2025-11-01 14:41:41 +0200

    Update MainComponent.cpp

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -2,14 +2,13 @@
 
 MainComponent::MainComponent()
 {
-    guiPlayer.setAudioPlayer(&audioPlayer);
-
-    guiPlayer.onLoadFileRequest = [this]()
+    guiPlayer1.setAudioPlayer(&audioPlayer1);
+    guiPlayer1.onLoadFileRequest = [this]()
         {
             fileChooser = std::make_unique<juce::FileChooser>(
-                "Select an audio file...",
+                "Select an audio file for Track 1...",
                 juce::File{},
-                "*.wav;*.mp3");
+                "*.wav;*.mp3;*.aiff;*.flac");
 
             fileChooser->launchAsync(
                 juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
@@ -18,14 +17,51 @@ MainComponent::MainComponent()
                     auto file = fc.getResult();
                     if (file.existsAsFile())
                     {
-                        audioPlayer.loadFile(file);
-                        guiPlayer.loadFileForWaveform(file);
-                        guiPlayer.updateFileInfoLabel(audioPlayer.getFileInfo());
-                        guiPlayer.updateSaveLabel(audioPlayer.getSave());
+                        audioPlayer1.loadFile(file);
+                        guiPlayer1.loadFileForWaveform(file);
                     }
                 });
         };
-    addAndMakeVisible(guiPlayer);
+    addAndMakeVisible(guiPlayer1);
+
+    guiPlayer2.setAudioPlayer(&audioPlayer2);
+    guiPlayer2.onLoadFileRequest = [this]()
+        {
+            fileChooser = std::make_unique<juce::FileChooser>(
+                "Select an audio file for Track 2...",
+                juce::File{},
+                "*.wav;*.mp3;*.aiff;*.flac");
+
+            fileChooser->launchAsync(
+                juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
+                [this](const juce::FileChooser& fc)
+                {
+                    auto file = fc.getResult();
+                    if (file.existsAsFile())
+                    {
+                        audioPlayer2.loadFile(file);
+                        guiPlayer2.loadFileForWaveform(file);
+                    }
+                });
+        };
+    addAndMakeVisible(guiPlayer2);
+
+    // إعداد خلاط الصوت الرئيسي
+    addAndMakeVisible(masterVolumeSlider);
+    masterVolumeSlider.setRange(0.0, 1.0, 0.01);
+    masterVolumeSlider.setValue(0.7);
+    masterVolumeSlider.setTextBoxStyle(juce::Slider::TextBoxRight, false, 60, 20);
+
+    addAndMakeVisible(masterVolumeLabel);
+    masterVolumeLabel.setText("Main Volume", juce::dontSendNotification);
+
+    addAndMakeVisible(balanceSlider);
+    balanceSlider.setRange(0.0, 1.0, 0.01);
+    balanceSlider.setValue(0.5);
+    balanceSlider.setTextBoxStyle(juce::Slider::TextBoxRight, false, 60, 20);
+
+    addAndMakeVisible(balanceLabel);
+    balanceLabel.setText("Balance", juce::dontSendNotification);
 
     setAudioChannels(0, 2);
 }
@@ -37,17 +73,48 @@ MainComponent::~MainComponent()
 
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
-    audioPlayer.prepareToPlay(samplesPerBlockExpected, sampleRate);
+    audioPlayer1.prepareToPlay(samplesPerBlockExpected, sampleRate);
+    audioPlayer2.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    audioPlayer.getNextAudioBlock(bufferToFill);
+    bufferToFill.clearActiveBufferRegion();
+
+    juce::AudioBuffer<float> tempBuffer1(bufferToFill.buffer->getNumChannels(), bufferToFill.numSamples);
+    juce::AudioBuffer<float> tempBuffer2(bufferToFill.buffer->getNumChannels(), bufferToFill.numSamples);
+
+    juce::AudioSourceChannelInfo tempInfo1(&tempBuffer1, 0, bufferToFill.numSamples);
+    juce::AudioSourceChannelInfo tempInfo2(&tempBuffer2, 0, bufferToFill.numSamples);
+
+    tempBuffer1.clear();
+    tempBuffer2.clear();
+
+    audioPlayer1.getNextAudioBlock(tempInfo1);
+    audioPlayer2.getNextAudioBlock(tempInfo2);
+
+    float balanceValue = (float)balanceSlider.getValue();
+    float gain1 = 1.0f - balanceValue;
+    float gain2 = balanceValue;
+
+    tempBuffer1.applyGain(gain1);
+    tempBuffer2.applyGain(gain2);
+
+    for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+    {
+        bufferToFill.buffer->addFrom(channel, bufferToFill.startSample,
+            tempBuffer1, channel, 0, bufferToFill.numSamples);
+        bufferToFill.buffer->addFrom(channel, bufferToFill.startSample,
+            tempBuffer2, channel, 0, bufferToFill.numSamples);
+    }
+
+    bufferToFill.buffer->applyGain((float)masterVolumeSlider.getValue());
 }
 
 void MainComponent::releaseResources()
 {
-    audioPlayer.releaseResources();
+    audioPlayer1.releaseResources();
+    audioPlayer2.releaseResources();
 }
 
 void MainComponent::paint(juce::Graphics& g)
@@ -57,9 +124,15 @@ void MainComponent::paint(juce::Graphics& g)
 
 void MainComponent::resized()
 {
-    guiPlayer.setBounds(getLocalBounds());
+    auto area = getLocalBounds();
+
+    auto controlArea = area.removeFromBottom(80);
+    masterVolumeLabel.setBounds(controlArea.removeFromLeft(100).reduced(5));
+    masterVolumeSlider.setBounds(controlArea.removeFromLeft(150).reduced(5));
+    balanceLabel.setBounds(controlArea.removeFromLeft(100).reduced(5));
+    balanceSlider.setBounds(controlArea.removeFromLeft(150).reduced(5));
+
+    auto halfWidth = area.getWidth() / 2;
+    guiPlayer1.setBounds(area.removeFromLeft(halfWidth));
+    guiPlayer2.setBounds(area);
 }
-
-
-
-
