01f6f518d83d7b11a9b3f005d5815298084595f3
MohamedAyser1 <20240476@stud.fci-cu.edu.eg>
2025-11-03 11:46:24 -0800

    Add playlist GUI and refactor player layout
    Refactor MainComponent to include playlist functionality and improve layout.

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -2,6 +2,7 @@
 
 MainComponent::MainComponent()
 {
+    // ---------- Player 1 ----------
     guiPlayer1.setAudioPlayer(&audioPlayer1);
     guiPlayer1.onLoadFileRequest = [this]()
         {
@@ -19,12 +20,13 @@ MainComponent::MainComponent()
                     {
                         audioPlayer1.loadFile(file);
                         guiPlayer1.loadFileForWaveform(file);
-						guiPlayer1.updateFileInfoLabel(audioPlayer1.getFileInfo());
+                        guiPlayer1.updateFileInfoLabel(audioPlayer1.getFileInfo());
                     }
                 });
         };
     addAndMakeVisible(guiPlayer1);
 
+    // ---------- Player 2 ----------
     guiPlayer2.setAudioPlayer(&audioPlayer2);
     guiPlayer2.onLoadFileRequest = [this]()
         {
@@ -42,14 +44,64 @@ MainComponent::MainComponent()
                     {
                         audioPlayer2.loadFile(file);
                         guiPlayer2.loadFileForWaveform(file);
-						guiPlayer2.updateFileInfoLabel(audioPlayer2.getFileInfo());
+                        guiPlayer2.updateFileInfoLabel(audioPlayer2.getFileInfo());
                     }
                 });
-			
         };
     addAndMakeVisible(guiPlayer2);
 
+    // ---------- Playlist ----------
+    addAndMakeVisible(playlistGUI);
+    playlistGUI.onAddToPlaylistRequest = [this]()
+        {
+            fileChooser = std::make_unique<juce::FileChooser>(
+                "Select audio files...",
+                juce::File{},
+                "*.wav;*.mp3");
 
+            fileChooser->launchAsync(
+                juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectMultipleItems,
+                [this](const juce::FileChooser& fc)
+                {
+                    juce::Array<juce::File> chosen = fc.getResults();
+                    for (auto& file : chosen)
+                        audioPlayer.addFileToPlaylist(file);
+
+                    playlistGUI.updatePlaylistDisplay(audioPlayer.getPlaylist());
+                });
+        };
+
+   
+    playlistGUI.onTrackSelected = [this](int index)
+        {
+            auto playlist = audioPlayer.getPlaylist();
+            if (index >= 0 && index < (int)playlist.size())
+            {
+                juce::File file = playlist[index];
+
+               
+                if (!audioPlayer1.isPlaying())
+                {
+                    audioPlayer1.loadFile(file);
+                    guiPlayer1.loadFileForWaveform(file);
+                    guiPlayer1.updateFileInfoLabel(audioPlayer1.getFileInfo());
+                }
+                else
+                {
+                    audioPlayer2.loadFile(file);
+                    guiPlayer2.loadFileForWaveform(file);
+                    guiPlayer2.updateFileInfoLabel(audioPlayer2.getFileInfo());
+                }
+            }
+        };
+
+    playlistGUI.onClearPlaylistRequest = [this]()
+        {
+            audioPlayer.clearPlaylist();
+            playlistGUI.updatePlaylistDisplay(audioPlayer.getPlaylist());
+        };
+
+   
     addAndMakeVisible(masterVolumeSlider);
     masterVolumeSlider.setRange(0.0, 1.0, 0.01);
     masterVolumeSlider.setValue(0.7);
@@ -66,35 +118,8 @@ MainComponent::MainComponent()
     addAndMakeVisible(balanceLabel);
     balanceLabel.setText("Balance", juce::dontSendNotification);
 
+    
     setAudioChannels(0, 2);
-	guiPlayer.onAddToPlaylistRequest = [this]()
-{
-    fileChooser = std::make_unique<juce::FileChooser>(
-        "Select audio files...",
-        juce::File{},
-        "*.wav;*.mp3");
-
-    fileChooser->launchAsync(
-        juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectMultipleItems,
-        [this](const juce::FileChooser& fc)
-        {
-            juce::Array<juce::File> chosen = fc.getResults();
-            for (auto& file : chosen)
-                audioPlayer.addFileToPlaylist(file);
-
-            guiPlayer.updatePlaylistDisplay(audioPlayer.getPlaylist());
-        });
-};
-	guiPlayer.onTrackSelected = [this](int index)
-{
-    audioPlayer.playTrack(index);
-};
-	guiPlayer.onClearPlaylistRequest = [this]()
-{
-    audioPlayer.clearPlaylist();
-    guiPlayer.updatePlaylistDisplay(audioPlayer.getPlaylist());
-};
-
 }
 
 MainComponent::~MainComponent()
@@ -118,9 +143,6 @@ void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer
     juce::AudioSourceChannelInfo tempInfo1(&tempBuffer1, 0, bufferToFill.numSamples);
     juce::AudioSourceChannelInfo tempInfo2(&tempBuffer2, 0, bufferToFill.numSamples);
 
-    tempBuffer1.clear();
-    tempBuffer2.clear();
-
     audioPlayer1.getNextAudioBlock(tempInfo1);
     audioPlayer2.getNextAudioBlock(tempInfo2);
 
@@ -131,12 +153,10 @@ void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer
     tempBuffer1.applyGain(gain1);
     tempBuffer2.applyGain(gain2);
 
-    for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+    for (int ch = 0; ch < bufferToFill.buffer->getNumChannels(); ++ch)
     {
-        bufferToFill.buffer->addFrom(channel, bufferToFill.startSample,
-            tempBuffer1, channel, 0, bufferToFill.numSamples);
-        bufferToFill.buffer->addFrom(channel, bufferToFill.startSample,
-            tempBuffer2, channel, 0, bufferToFill.numSamples);
+        bufferToFill.buffer->addFrom(ch, bufferToFill.startSample, tempBuffer1, ch, 0, bufferToFill.numSamples);
+        bufferToFill.buffer->addFrom(ch, bufferToFill.startSample, tempBuffer2, ch, 0, bufferToFill.numSamples);
     }
 
     bufferToFill.buffer->applyGain((float)masterVolumeSlider.getValue());
@@ -157,18 +177,21 @@ void MainComponent::resized()
 {
     auto area = getLocalBounds();
 
-    auto controlArea = area.removeFromBottom(80);
-    masterVolumeLabel.setBounds(controlArea.removeFromLeft(100).reduced(5));
-    masterVolumeSlider.setBounds(controlArea.removeFromLeft(150).reduced(5));
-    balanceLabel.setBounds(controlArea.removeFromLeft(100).reduced(5));
-    balanceSlider.setBounds(controlArea.removeFromLeft(150).reduced(5));
+   
+    auto playlistArea = area.removeFromBottom(180);
 
+    
     auto halfWidth = area.getWidth() / 2;
-    guiPlayer1.setBounds(area.removeFromLeft(halfWidth));
-    guiPlayer2.setBounds(area);
+    guiPlayer1.setBounds(area.removeFromLeft(halfWidth).reduced(10));
+    guiPlayer2.setBounds(area.reduced(10));
+
+    
+    playlistGUI.setBounds(playlistArea.reduced(10));
+
+   
+    auto controls = playlistArea.removeFromTop(40);
+    masterVolumeLabel.setBounds(controls.removeFromLeft(100).reduced(5));
+    masterVolumeSlider.setBounds(controls.removeFromLeft(150).reduced(5));
+    balanceLabel.setBounds(controls.removeFromLeft(100).reduced(5));
+    balanceSlider.setBounds(controls.removeFromLeft(150).reduced(5));
 }
-
-
-
-
-
