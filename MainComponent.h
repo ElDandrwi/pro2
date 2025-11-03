34e14e7441b17a2fb10bc87c5bd0cae79e9b9b04
MohamedAyser1 <20240476@stud.fci-cu.edu.eg>
2025-11-03 11:46:54 -0800

    Add PlaylistGUI to MainComponent

--- MainComponent.h
+++ MainComponent.h
@@ -3,6 +3,7 @@
 #include <JuceHeader.h>
 #include "PlayerAudio.h"
 #include "PlayerGUI.h"
+#include "PlaylistGUI.h"
 
 class MainComponent : public juce::AudioAppComponent
 {
@@ -18,21 +19,23 @@ public:
     void resized() override;
 
 private:
+    
     PlayerAudio audioPlayer1;
     PlayerAudio audioPlayer2;
     PlayerGUI   guiPlayer1;
     PlayerGUI   guiPlayer2;
-PlayerAudio audioPlayer;
-PlayerGUI guiPlayer;
 
+   
+    PlayerAudio audioPlayer;
+    PlaylistGUI playlistGUI;
+
+   
     juce::Slider masterVolumeSlider;
-    juce::Label masterVolumeLabel;
-
+    juce::Label  masterVolumeLabel;
     juce::Slider balanceSlider;
-    juce::Label balanceLabel;
+    juce::Label  balanceLabel;
 
     std::unique_ptr<juce::FileChooser> fileChooser;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
-
